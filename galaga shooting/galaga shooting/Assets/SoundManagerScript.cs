4fbd7bdd74cb2633b37a8fbc4922ed12d00ad44a
Snehil <snehilmaknojia7@gmail.com>
2018-10-04 22:31:07 -0500

    Add bases project

---
+++ galaga shooting/galaga shooting/Assets/SoundManagerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour {
+
+    public static AudioClip FireSound;
+    static AudioSource audioSrc;
+	// Use this for initialization
+	void Start () {
+
+        FireSound = Resources.Load<AudioClip>("Blast");
+        audioSrc = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void PlaySound (string clip)
+    {
+        switch(clip)
+        {
+            case "1":
+                audioSrc.PlayOneShot(FireSound);
+                break;
+        }
+    }
+}
