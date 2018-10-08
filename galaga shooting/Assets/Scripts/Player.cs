8bdacb2f1cfdd715e08222c533263b5c2dbd878b
Snehil <snehilmaknojia7@gmail.com>
2018-10-08 16:36:53 -0500

    none

---
+++ galaga shooting/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 20.0f;
+
+    public Rigidbody2D player;
+
+
+	// Use this for initialization
+	void Start () {
+        player = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MovePlayer();
+	}
+    
+    public void MovePlayer()
+    {
+        player.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * moveSpeed;
+
+    }
+}
