2e659172e154642d10c765036a296b7e5105623b
ArinStephen <arinsteph@gmail.com>
2018-11-06 17:16:34 -0600

    eye movement script
    Added eye movement

---
+++ galaga shooting/Assets/Galaxia Sprite Pack #1/Boss_1/Scripts/EyeMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeMovement : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 playerPosition;
+    private Vector3 currentPos;
+    private Vector3 moveDirection;
+    private float eyeSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentPos = transform.position;
+        playerPosition = player.transform.position;
+        moveDirection = (playerPosition - currentPos).normalized;
+        
+        eyeSpeed = 6.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.position += moveDirection * eyeSpeed * Time.deltaTime;
+        if(transform.position.x >= 10.5)
+        {
+            currentPos = transform.position;
+            playerPosition = player.transform.position;
+            transform.LookAt(playerPosition);
+            moveDirection = (playerPosition - currentPos).normalized;
+        }
+        else if(transform.position.x <= -9.5)
+        {
+            currentPos = transform.position;
+            playerPosition = player.transform.position;
+            transform.LookAt(playerPosition);
+            moveDirection = (playerPosition - currentPos).normalized;
+        }
+        else if(transform.position.y >= 5.5)
+        {
+            currentPos = transform.position;
+            playerPosition = player.transform.position;
+            transform.LookAt(playerPosition);
+            moveDirection = (playerPosition - currentPos).normalized;
+        }
+        else if(transform.position.y <= -5.5)
+        {
+            currentPos = transform.position;
+            playerPosition = player.transform.position;
+            transform.LookAt(playerPosition);
+            moveDirection = (playerPosition - currentPos).normalized;
+        }
+        
+    }
+}
