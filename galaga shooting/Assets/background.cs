c9c00d119944b85b8b301367c2df113c425114d3
Snehil <snehilmaknojia7@gmail.com>
2018-11-06 19:28:07 -0600

    added new background that has infinte scrolling

---
+++ galaga shooting/Assets/background.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background : MonoBehaviour {
+    public float speed;
+    Vector3 startPOS;
+	// Use this for initialization
+	void Start () {
+        startPOS = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate((new Vector3(0, -1, 0)) * speed * Time.deltaTime);
+        if (transform.position.y < -24.48)
+            transform.position = startPOS;
+    }
+}
