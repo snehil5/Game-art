102369f9129624b9ce379d040c6d1cebe9b7f269
Snehil <snehilmaknojia7@gmail.com>
2018-11-13 15:42:00 -0600

    added scences and files changed to other folder

---
+++ galaga shooting/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	float maxSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+		Vector3 pos = transform.position;
+
+		pos.y += Input.GetAxis ("Vertical") * maxSpeed * Time.deltaTime;
+
+		transform.position = pos;
+
+		pos.x += Input.GetAxis ("Horizontal") * maxSpeed * Time.deltaTime;
+
+		transform.position = pos;
+	}
+}
---
+++ galaga shooting/Assets/asteroid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asteroid : MonoBehaviour {
+    public GameObject asteroidPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Destroy(Instantiate(asteroidPrefab,transform.position,)
+	}
+
+
+}
