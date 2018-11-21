8f67e011e72a0861d729ddd1e5e373d1b536585e
Snehil <snehilmaknojia7@gmail.com>
2018-11-20 19:40:05 -0600

    boundary added by iptihar

--- galaga shooting/Assets/Scripts/PlayerMovement.cs
+++ galaga shooting/Assets/Scripts/PlayerMovement.cs
@@ -5,25 +5,47 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	float maxSpeed = 5f;
-
-	// Use this for initialization
-	void Start () {
+    float shipBoundary = 0.5f;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 
 
-		Vector3 pos = transform.position;
 
-		pos.y += Input.GetAxis ("Vertical") * maxSpeed * Time.deltaTime;
 
-		transform.position = pos;
+        Vector3 pos = transform.position;
 
-		pos.x += Input.GetAxis ("Horizontal") * maxSpeed * Time.deltaTime;
+        pos.y += Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;
 
-		transform.position = pos;
-	}
+        transform.position = pos;
+
+        pos.x += Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime;
+
+        if (pos.y + shipBoundary > Camera.main.orthographicSize)
+        {
+            pos.y = Camera.main.orthographicSize - shipBoundary;
+        }
+        if (pos.y - shipBoundary < -Camera.main.orthographicSize)
+        {
+            pos.y = -Camera.main.orthographicSize + shipBoundary;
+        }
+
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float widthOrtho = Camera.main.orthographicSize * screenRatio;
+
+        if (pos.x + shipBoundary > widthOrtho)
+        {
+            pos.x = widthOrtho - shipBoundary;
+        }
+        if (pos.x - shipBoundary < -widthOrtho)
+        {
+            pos.x = -widthOrtho + shipBoundary;
+        }
+
+        transform.position = pos;
+    }
 }
