ce279e972e30f3afc6ad24f23c32dcb148db6423
Powerrice1 <thenguyenshow@gmail.com>
2018-11-25 10:55:41 -0600

    update shooting into master

--- galaga shooting/Assets/Scripts/MoveForward.cs
+++ galaga shooting/Assets/Scripts/MoveForward.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour {
 
-	float maxSpeed = 5f;
+	public float maxSpeed = 15f;
 	
 	// Update is called once per frame
 	void Update () {
--- galaga shooting/Assets/Scripts/SelfDestruct.cs
+++ galaga shooting/Assets/Scripts/SelfDestruct.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SelfDestruct : MonoBehaviour {
+public class SelfDestruct : MonoBehaviour
+{
 
     public float timer = 5f;
-	
-	// Update is called once per frame
-	void Update () {
+    public GameObject deathFX;
+
+    // Update is called once per frame
+    void Update()
+    {
         timer -= Time.deltaTime;
 
-        if(timer <= 0)
+        if (timer <= 0)
         {
             Destroy(gameObject);
         }
-	}
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            Destroy(Instantiate(deathFX, transform.position, transform.rotation), .2f);
+            Destroy(gameObject);
+        }
+    }
 }
