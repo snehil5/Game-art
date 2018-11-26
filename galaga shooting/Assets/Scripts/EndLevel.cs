66924c2b3efddf966caa563c7c3749fd02cf1b6e
Powerrice1 <36394294+Powerrice1@users.noreply.github.com>
2018-11-26 13:52:06 -0600

    updating enemies for lvl 1

--- galaga shooting/Assets/Scripts/EndLevel.cs
+++ galaga shooting/Assets/Scripts/EndLevel.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class EndLevel : MonoBehaviour {
 
     public PlayerSpawner score;
-
+    public int winningScore;
     private void OnGUI()
     {
-        if (score.getScore() >= 115)
+        if (score.getScore() >= winningScore)
         {
             
             GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 25, 100, 50), "Level Over! \nTotal Score:" + score.getScore());
