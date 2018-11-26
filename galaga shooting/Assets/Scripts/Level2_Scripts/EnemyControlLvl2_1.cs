0ff0f577e863b475cb285dad59a8ddc3ac696906
Snehil <snehilmaknojia7@gmail.com>
2018-11-26 12:21:26 -0600

    stoped mini boss movement

--- galaga shooting/Assets/Scripts/Level2_Scripts/EnemyControlLvl2_1.cs
+++ galaga shooting/Assets/Scripts/Level2_Scripts/EnemyControlLvl2_1.cs
@@ -6,6 +6,7 @@ public class EnemyControlLvl2_1 : MonoBehaviour
 {
     DamageHandler health;
     float speed;
+    int noMove = 0;
 
     // Use this for initialization
     void Start()
@@ -17,21 +18,38 @@ public class EnemyControlLvl2_1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
-        Vector2 position = transform.position;
-
-        position = new Vector2(position.x, position.y - speed * Time.deltaTime);
-        transform.position = position;
-
-        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
-
-        if (transform.position.y < min.y)
+        Debug.Log("the tag is " + gameObject.tag);
+        Debug.Log(transform.position.y + " is the y postiion" );
+        if (gameObject.tag == "miniBoss" && noMove == 0)
         {
-            Destroy(gameObject);
+            updateMini();
+        }
+        else if(gameObject.tag != "miniBoss")
+        {
+            Vector2 position = transform.position;
+
+            position = new Vector2(position.x, position.y - speed * Time.deltaTime);
+            transform.position = position;
+
+            Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+
+            if (transform.position.y < min.y)
+            {
+                Destroy(gameObject);
+            }
         }
     }
     private void updateMini()
     {
+        Vector2 position = transform.position;
+
+        position = new Vector2(position.x, position.y - speed * Time.deltaTime);
+        transform.position = position;
+       
+        if (transform.position.y < 3.6 )
+        {
+            noMove++;
+        }
 
     }
 }
