e0bad4901d1280decc82bb5b9b2d3d9c0120b11e
Powerrice1 <36394294+Powerrice1@users.noreply.github.com>
2018-11-26 21:51:05 -0600

    cutscene

---
+++ galaga shooting/Assets/Scripts/TypeTextComponent.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TypeTextComponent : MonoBehaviour
+{
+    public delegate void OnComplete();
+
+    [SerializeField]
+    private float defaultSpeed = 0.05f;
+
+    private Text label;
+    private string currentText;
+    private string finalText;
+    private Coroutine typeTextCoroutine;
+
+    private OnComplete onCompleteCallback;
+
+    private void Init()
+    {
+        if (label == null)
+            label = GetComponent<Text>();
+    }
+
+    public void Awake()
+    {
+        Init();
+    }
+
+    public void SetText(string text, float speed = -1)
+    {
+        Init();
+
+        defaultSpeed = speed > 0 ? speed : defaultSpeed;
+        finalText = ReplaceSpeed(text);
+        label.text = "";
+
+        if (typeTextCoroutine != null)
+        {
+            StopCoroutine(typeTextCoroutine);
+        }
+
+        typeTextCoroutine = StartCoroutine(TypeText(text));
+    }
+
+    public void SkipTypeText()
+    {
+        if (typeTextCoroutine != null)
+            StopCoroutine(typeTextCoroutine);
+        typeTextCoroutine = null;
+
+        label.text = finalText;
+
+        if (onCompleteCallback != null)
+            onCompleteCallback();
+    }
+
+    public IEnumerator TypeText(string text)
+    {
+        currentText = "";
+
+        var len = text.Length;
+        var speed = defaultSpeed;
+        var tagOpened = false;
+        var tagType = "";
+        for (var i = 0; i < len; i++)
+        {
+            if (text[i] == '[' && i + 6 < len && text.Substring(i, 7).Equals("[speed="))
+            {
+                var parseSpeed = "";
+                for (var j = i + 7; j < len; j++)
+                {
+                    if (text[j] == ']')
+                        break;
+                    parseSpeed += text[j];
+                }
+
+                if (!float.TryParse(parseSpeed, out speed))
+                    speed = 0.05f;
+
+                i += 8 + parseSpeed.Length - 1;
+                continue;
+            }
+
+            currentText += text[i];
+            label.text = currentText + (tagOpened? string.Format("</{0}>", tagType) : "");
+            yield return new WaitForSeconds(speed);
+        }
+
+        typeTextCoroutine = null;
+
+        if (onCompleteCallback != null)
+            onCompleteCallback();
+    }
+
+    private string ReplaceSpeed(string text)
+    {
+        var result = "";
+        var len = text.Length;
+        for (var i = 0; i < len; i++)
+        {
+            if (text[i] == '[' && i + 6 < len && text.Substring(i, 7).Equals("[speed="))
+            {
+                var speedLength = 0;
+                for (var j = i + 7; j < len; j++)
+                {
+                    if (text[j] == ']')
+                        break;
+                    speedLength++;
+                }
+
+                i += 8 + speedLength - 1;
+                continue;
+            }
+
+            result += text[i];
+        }
+
+        return result;
+    }
+
+    public bool IsSkippable()
+    {
+        return typeTextCoroutine != null;
+    }
+
+    public void SetOnComplete(OnComplete onComplete)
+    {
+        onCompleteCallback = onComplete;
+    }
+
+}
+
+public static class TypeTextComponentUtility
+{
+
+    public static void TypeText(this Text label, string text, float speed = 0.05f, TypeTextComponent.OnComplete onComplete = null)
+    {
+        var typeText = label.GetComponent<TypeTextComponent>();
+        if (typeText == null)
+        {
+            typeText = label.gameObject.AddComponent<TypeTextComponent>();
+        }
+
+        typeText.SetText(text, speed);
+        typeText.SetOnComplete(onComplete);
+    }
+
+    public static bool IsSkippable(this Text label)
+    {
+        var typeText = label.GetComponent<TypeTextComponent>();
+        if (typeText == null)
+        {
+            typeText = label.gameObject.AddComponent<TypeTextComponent>();
+        }
+
+        return typeText.IsSkippable();
+    }
+
+    public static void SkipTypeText(this Text label)
+    {
+        var typeText = label.GetComponent<TypeTextComponent>();
+        if (typeText == null)
+        {
+            typeText = label.gameObject.AddComponent<TypeTextComponent>();
+        }
+
+        typeText.SkipTypeText();
+    }
+
+}
---
+++ galaga shooting/Assets/Scripts/uGUI.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class uGUI : MonoBehaviour
+{
+    public Text text;
+    public Text capName, bossName;
+    public Image dialogueIMG;
+    public Sprite captain, boss;
+    public GameObject bossHead, nextButton;
+    public bool sprite = true;
+    public bool talkerName = true;
+    private Queue<string> scripts = new Queue<string>();
+
+    public void Start()
+    {
+        //Vincent
+        scripts.Enqueue("I know this is short notice");
+        scripts.Enqueue("But without our best captain, we're doomed!");
+        //Boss
+        scripts.Enqueue("Why hello there, you will all die!");
+        //Vincent
+        scripts.Enqueue("If it gets a hold of us, our timeline will be erased. [speed=0.2]:(");
+        //Boss
+        scripts.Enqueue("Will the captain not speak? I see, it's the \'silent main character\' trope.");
+        //Vincent
+        scripts.Enqueue("You will be known as the universe's greatest champion if you win! Thats enough motivation right?");
+        //Boss
+        scripts.Enqueue("See ya in level 3!");
+        //Vincent
+        scripts.Enqueue("Captains, roll out!");
+        ShowScript();
+    }
+
+    private void ShowScript()
+    {
+        if (scripts.Count <= 0)
+        {
+            nextButton.SetActive(true);
+            return;
+        }
+
+        text.TypeText(scripts.Dequeue(), onComplete: () => Debug.Log("TypeText Complete"));
+    }
+
+    public void OnClickWindow()
+    {
+        if (text.IsSkippable())
+        {
+            text.SkipTypeText();
+        }
+        else
+        {
+            ShowScript();
+            if (sprite)
+            {
+                dialogueIMG.sprite = boss;
+                bossName.gameObject.SetActive(true);
+                capName.gameObject.SetActive(false);
+                bossHead.SetActive(true);
+                sprite = false;
+            }
+            else
+            {
+                dialogueIMG.sprite = captain;
+                capName.gameObject.SetActive(true);
+                bossName.gameObject.SetActive(false);
+                sprite = true;
+            }
+        }
+    }
+}
